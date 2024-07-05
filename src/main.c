cf97bda54645824f799fab274930fa60f4a96d9d
Paweł Lidwin <lidwinpawel@gmail.com>
2024-07-05 14:29:26 +0200

    initial project setup

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#define PROJECT_NAME "GalaxyCommunication-wine"
+
+int main(int argc, char **argv) {
+    printf("This is project %s.\n", PROJECT_NAME);
+    return 0;
+}
---
+++ unixlib/main.c
@@ -0,0 +1,10 @@
+#include <windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
+    switch (fdwReason) {
+        case DLL_PROCESS_ATTACH:
+            DisableThreadLibraryCalls(hinstDLL);
+            break;
+    }
+    return TRUE;
+}
